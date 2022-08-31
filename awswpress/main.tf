ed8a3db098537002ff2a20223c4c04de7743fa9e
Matthew Ashman <matthewiashman@gmail.com>
2022-08-31 10:05:19 +0100

    Add pinging to security group in main.tf

--- awswpress/main.tf
+++ awswpress/main.tf
@@ -37,6 +37,14 @@ resource "aws_security_group" "project-iac-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  // To Allow Pinging
+  ingress {
+    from_port = 8
+    protocol = "icmp"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port       = 0
     to_port         = 0
